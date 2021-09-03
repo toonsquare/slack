cad57a34be5f4196c89bca2bae6227499a53f340
branden <cto@toonsquare.co>
2021-09-03 17:36:29 +0900

    Add try catch

--- src/_origin.js
+++ src/_origin.js
@@ -1,8 +1,13 @@
 var origin = 'https://slack.com'
 
 // allow configuring the url if steno is running on a non-standard port/host
-if (process.env.STENO_URL) {
-  origin = process.env.STENO_URL
+try{
+  if (process.env.STENO_URL) {
+    origin = process.env.STENO_URL
+  }
+}catch (e) {
+  console.log(e);
 }
+
 
 module.exports = origin
